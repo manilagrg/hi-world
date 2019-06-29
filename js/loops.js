6fc216ce17fa592720d413c46216d7b87ac3e416
manila <manilagrg@gmail.com>
2019-06-29 22:54:32 +0545

    add loops

---
+++ js/loops.js
@@ -0,0 +1,34 @@
+
+
+//initializer is to set the number amounts etc in computer programe so that it ready to work.
+/* to start up frist you need to give variables 
+*/
+
+var ans = 0
+
+for( ans; ans <= 20; ans = ans +2){
+    console.log(ans)
+    if(ans % 3 == 0 ){
+        console.log("Odd number")
+    }
+}
+
+
+
+for( var n = 0; n < 20; n +1 ){
+    console.log(n)
+    if( n / 5 == 0 ){
+        console.log("Divisible by five")
+    }
+}
+
+for(var m = 0; m < 20; m ++){
+    console.log(m, m * 3)
+}
+
+
+    
+
+
+
+
